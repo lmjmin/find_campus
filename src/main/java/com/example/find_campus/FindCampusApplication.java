62e1c9294205f78867ee97153a2a32931db413a7
이민주 <minju200308@gmail.com>
2026-05-08 21:38:12 +0900

    test

---
+++ src/main/java/com/example/find_campus/FindCampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.find_campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/find_campus/FindCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.find_campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
